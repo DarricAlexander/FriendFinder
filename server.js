4a4504c0c8d245d12846c69539aeaf8db2fe8ded
Darric <darricalexander@gmail.com>
2018-11-29 11:42:07 -0700

    working

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var path = require('path')
+var express = require("express");
 
-var app = express()
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
 
-var PORT = process.env.PORT || 8080
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
 
-// middleware
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-
-
-app.listen(PORT, function (err) {
-    if (err) throw err
-    console.log('listening on port ' + PORT)
-})
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
